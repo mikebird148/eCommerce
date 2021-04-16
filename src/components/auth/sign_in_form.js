e76f176220ea1d7aa96d3204a511098ed2cdd9e4
Mike Bird <mikebird148@gmail.com>
2021-04-16 10:02:42 -0700

    finalized initial sign up page

--- src/components/auth/sign_in_form.js
+++ src/components/auth/sign_in_form.js
@@ -50,7 +50,7 @@ class SignInForm extends Component {
                 <div className='sign-in-form__line'></div>
                 <Field 
                     className='sign-in-form__login' 
-                    onClick={() => console.log("submit")}
+                    onClick={() => history.push('/account')}
                     type='submit'    
                     title='Login' 
                     name='login' 
--- src/components/auth/sign_up.js
+++ src/components/auth/sign_up.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 
+import SignUpForm from './sign_up_form';
+import PageTitle from '../page-title';
+
 class SignUp extends Component {
+    onSubmit = (fields) => {
+        console.log(fields);
+    }
+
     render() {
         return(
             <div className='sign-up'>
-                Sign up
+                <PageTitle className='sign-up__page-title' title='Register' />
+                <SignUpForm onSubmit={this.onSubmit} className='sign-up__form' />
             </div>
         )
     }
