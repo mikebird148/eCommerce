7649ba3fa3dde2b19f1da64ed2f335531f210366
Mike Bird <mikebird148@gmail.com>
2021-04-24 19:57:46 -0700

    finished account information page and started shop component

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -14,6 +14,7 @@ import Layout from "./components/layout";
 import SignIn from "./components/auth/sign_in";
 import SignUp from "./components/auth/sign_up";
 import Account from "./components/account/account";
+import Shop from "./components/shop/shop";
 
 function main() {
   ReactDOM.render(
@@ -25,6 +26,7 @@ function main() {
             <Route path='/signin' exact component={SignIn} />
             <Route path='/signup' exact component={SignUp} />
             <Route path='/account' exact component={Account} />
+            <Route path='/shop' exact component={Shop} />
           </Switch>
         </Layout>
       </Router>
--- src/components/account/accountInformation.js
+++ src/components/account/accountInformation.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 
+import PageTitle from '../page-title';
+import AccountInformationForm from '../account/accountInformationForm';
+
 class AccountInformation extends Component {
+    
+    onSubmit = (fields) => {
+        console.log(fields);
+    }
+
     render() {
         return (
-            <div>
-                account info
+            <div className="account-information">
+                <PageTitle className="account-information" title="Account Information" />
+                <AccountInformationForm onSubmit={this.onSubmit} className="account-information__form" />
             </div>
         )
     }
